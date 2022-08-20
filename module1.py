05c290b6835150ea691aafffbc4b0a9f46920c63
Yevhen <tsaplay13@gmail.com>
2022-08-21 01:43:14 +0300

    Divided PP_l3.2 in 3 modules

--- module1.py
+++
@@ -1,8 +0,0 @@
-class Person:
-    def __init__(self, name, surname, year):
-        self.name = name
-        self.surname = surname
-        self.year = year
-
-    def __str__(self):
-        return f'{self.surname} {self.name} Year: {self.year}'
