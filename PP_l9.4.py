f8964991358127ffceb7e10399eb1cc094cc77c7
Yevhen <tsaplay13@gmail.com>
2022-09-08 01:39:16 +0300

    Added a decorator with parameters for timing the function, whose parameters are: how many times you need to run the decorated function and in which file to save the results timing. The goal is to time the decorated function.

---
+++ PP_l9.4.py
@@ -0,0 +1,29 @@
+import time
+
+
+def decorator(n, adress):
+    def wrapped(f):
+        def my_function(*args):
+            count = 1
+            start = time.time()
+            while count <= n:
+                f(*args)
+                count += 1
+            end = time.time()
+            file = open(adress, "w")
+            file.write(f'{n} times per {end - start} s.')
+            file.close()
+        return my_function
+    return wrapped
+
+
+@decorator(10_000_000, 'timing.txt')
+def mul(a, b):
+    return a * b
+
+
+mul(361233, 70914353)
+mul(36123, 709213)
+mul(36345, 709213)
+mul(36123, 70921)
+mul(362342, 70932423)
